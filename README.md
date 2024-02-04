d0411b12a5ce006ed707ac9054e603537428aaf8
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-02-04 22:51:13 +0530

    setting up app

---
+++ README.md
@@ -0,0 +1,14 @@
+# welcome react!!
+# parcel 
+- dev build / setup
+- local server(localhost)
+- HMR -hot module replacement parcel uses file watching algo..
+- caching
+- image optimization
+- comprssing
+- bundling
+- consistent hashing
+- code split
+- minification
+- error handling
+- tree shaking -remove unused thing in code
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent=React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"}, React.createElement("h1",{id:"heading"}, "hi da  react la irundhu")));
     const root=ReactDOM.createRoot(document.getElementById("root"));
     root.render(parent)
