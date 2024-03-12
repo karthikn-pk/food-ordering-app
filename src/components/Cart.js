6efccf393d41a082d6d40e4095643f67b3516294
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-03-12 23:17:29 +0530

    clear cart button added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart=()=>{
 
     const cartItems=useSelector((store)=>store.cart.items);
+    const dispatch=useDispatch();
+    const handleClearCart=()=>{
+        dispatch(clearCart());
+        <h1>cart is empty</h1>
+    }
     return(
         <div className="text-center">
-            <h1 className="font-bold text-2xl">cart</h1>
+            <h1 className="font-bold text-2xl">cart  </h1> 
+            <button className="text-white bg-red-500 p-2 m-2 rounded-md" onClick={handleClearCart}>clear Cart</button>
+            {cartItems.length===0 && <h1>Cart is empty</h1>}
             <div className="w-6/12 m-auto">
                 <ItemList items={cartItems}/>
+
             </div>
         </div>
 
